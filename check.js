7e1671cb7bf44d2142c281aa4f0ae810b95c012f
unlqvt <lscharnweber92@gmail.com>
2022-04-05 20:19:04 +0200

    Added verification

---
+++ check.js
@@ -0,0 +1,22 @@
+var response = [{"Access allowed": allowed}];
+document.getElementById("response")
+
+function validate()
+{
+    if (navigator.userAgent.includes('unlqvt Client'))
+    {
+        response[0].allowed = 'true';
+    }
+    else
+    {
+        response[0].allowed = 'false';
+    }
+    var data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(response));
+    var dlAnchorElem = document.getElementById("downloadAnchorElem");
+    dlAnchorElem.setAttribute("href", data);
+    dlAnchorElem.setAttribute("download", "check.json");
+    dlAnchorElem.click();
+
+    
+}
+
